02d0b4e8a1ec448e59892e505f59edc3182b9c9f
TLCroatt <trishacroatt@gmail.com>
2020-12-12 15:40:44 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import About from "./pages"
+import './App.css';
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
